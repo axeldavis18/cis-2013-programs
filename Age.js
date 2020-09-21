e7910880abe330483dd414184efef4293c0a3123
axeldavis18 <70623832+axeldavis18@users.noreply.github.com>
2020-09-21 01:57:26 -0500

    Add files via upload

---
+++ Age.js
@@ -0,0 +1,10 @@
+var floatAge, floatMonth, floatFortnight, floatDays, floatWeeks;
+floatAge = parseFloat(prompt("Enter your age in years"));
+floatMonth = floatAge*12;
+floatFortnight = floatAge*26;
+floatDays = floatAge*356;
+floatWeeks = floatAge*52;
+alert("Days lived: " + floatDays + ", Weeks Lived: " + floatWeeks + ", Fortnights lived: " + floatFortnight + ", Months lived: " + floatMonth); 
+
+
+
